bc4ec56d4a1ef85bb787483b33733e560188969e
franklin yanez <yanfranve@gmail.com>
2024-01-05 14:58:25 -0500

    route

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpleadoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/empleados', [EmpleadoController::class, 'index'])->name('empleados.index');
 Route::get('/empleados/create', [EmpleadoController::class, 'create'])->name('empleados.create');
-Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados/store');
+Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados.store');
 Route::get('/empleados/{empleado}', [EmpleadoController::class, 'show'])->name('empleados.show');
 Route::put('/empleados/{empleado}', [EmpleadoController::class, 'update'])->name('empleados.update');
